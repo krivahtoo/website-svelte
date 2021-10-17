55a5464f313e196aa3c3b0324cb167dc1b07176c
Noah Too <krivahtoo@gmail.com>
2021-10-17 23:20:36 +0300

    Initial setup

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Router from 'svelte-spa-router';
+    import NavBar from './components/NavBar.svelte'
+    import Footer from './components/Footer.svelte'
+	import routes from './routes';
+</script>
+
+<main>
+  <NavBar />
+  <Router {routes} />
+  <Footer />
+</main>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,14 @@
+<div class="hero min-h-screen bg-base-200">
+  <div class="flex-col hero-content lg:flex-row-reverse">
+    <img src="https://picsum.photos/id/1005/600/600" class="mask mask-hexagon max-w-sm"> 
+    <div>
+      <h1 class="mb-5 text-5xl font-bold">
+            Noah Cheptoo
+          </h1> 
+      <p class="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p> 
+      <button class="btn btn-primary">Contact</button>
+    </div>
+  </div>
+</div>
